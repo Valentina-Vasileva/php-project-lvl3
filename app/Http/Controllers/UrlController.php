df07edea4aa9657252ecb1122fb88b81fbe9397b
Ariss <valentina.ways@yandex.ru>
2021-03-17 19:53:19 +0300

    add url checks to url index

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -16,7 +16,19 @@ class UrlController extends Controller
      */
     public function index()
     {
-        $urls = DB::table('urls')->get();
+        $latestChecks = DB::table('url_checks')
+            ->select(DB::raw('url_id, MAX(id) as last_check_id'))
+            ->groupBy('url_id');
+
+        $urls = DB::table('urls')
+            ->rightJoinSub($latestChecks, 'latest_checks', function ($join) {
+                $join->on('urls.id', '=', 'latest_checks.url_id');
+            })
+            ->join('url_checks', 'latest_checks.last_check_id', '=', 'url_checks.id')
+            ->select('urls.id as id', 'urls.name as name', 'url_checks.created_at as last_check', 'url_checks.status_code as status_code')
+            ->orderBy('id', 'asc')
+            ->get();
+
         return view('url.index', compact('urls'));
     }
 
--- resources/views/url/index.blade.php
+++ resources/views/url/index.blade.php
@@ -9,12 +9,16 @@
                     <tr>
                         <th scope="col">ID</th>
                         <th scope="col">Имя</th>
+                        <th scope="col">Последняя проверка</th>
+                        <th scope="col">Код ответа</th>
                     </tr>
                     @if ($urls)
                         @foreach ($urls as $url)
                             <tr>
                                 <td>{{ $url->id }}</td>
                                 <td scope="row"><a href="{{ route('urls.show', ['url' => $url->id]) }}">{{ $url->name }}</a></td>
+                                <td>{{ $url->last_check }}</td>
+                                <td>{{ $url->status_code }}</td>
                             </tr>
                         @endforeach
                     @endif
