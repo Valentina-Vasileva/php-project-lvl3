2a4718b936c74f4db0fb0512ee587ab2fb8c7528
Ariss <valentina.ways@yandex.ru>
2021-05-21 18:31:46 +0300

    fix table-responsive in indexes

--- resources/views/url/index.blade.php
+++ resources/views/url/index.blade.php
@@ -5,24 +5,26 @@
         <h1 class="mt-5 mb-3">Сайты</h1>
         <div class="row">
             <div class="col">
-                <table class="table table-hover table-bordered table-responsive">
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Последняя проверка</th>
-                        <th scope="col">Код ответа</th>
-                    </tr>
-                    @if ($urls)
-                        @foreach ($urls as $url)
-                            <tr>
-                                <td>{{ $url->id }}</td>
-                                <td scope="row"><a href="{{ route('urls.show', ['url' => $url->id]) }}">{{ $url->name }}</a></td>
-                                <td>{{ $url->last_check }}</td>
-                                <td>{{ $url->status_code }}</td>
-                            </tr>
-                        @endforeach
-                    @endif
-                </table>
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered">
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Последняя проверка</th>
+                            <th scope="col">Код ответа</th>
+                        </tr>
+                        @if ($urls)
+                            @foreach ($urls as $url)
+                                <tr>
+                                    <td>{{ $url->id }}</td>
+                                    <td scope="row"><a href="{{ route('urls.show', ['url' => $url->id]) }}">{{ $url->name }}</a></td>
+                                    <td>{{ $url->last_check }}</td>
+                                    <td>{{ $url->status_code }}</td>
+                                </tr>
+                            @endforeach
+                        @endif
+                    </table>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/url/show.blade.php
+++ resources/views/url/show.blade.php
@@ -17,20 +17,22 @@
         <h1 class="mt-5 mb-3">Сайт: {{ $url->name }}</h1>
         <div class="row">
             <div class="col">
-                <table class="table table-hover table-bordered table-responsive">
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Дата создания</th>
-                        <th scope="col">Дата обновления</th>
-                    </tr>
-                    <tr>
-                        <td>{{ $url->id }}</td>
-                        <td scope="row">{{ $url->name }}</td>
-                        <td>{{ $url->created_at }}</td>
-                        <td>{{ $url->updated_at }}</td>
-                    </tr>
-                </table>
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered">
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Дата создания</th>
+                            <th scope="col">Дата обновления</th>
+                        </tr>
+                        <tr>
+                            <td>{{ $url->id }}</td>
+                            <td scope="row">{{ $url->name }}</td>
+                            <td>{{ $url->created_at }}</td>
+                            <td>{{ $url->updated_at }}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
         <h2 class="mt-5 mb-3">Проверки</h2>
