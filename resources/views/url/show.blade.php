5f6e2b4aaab1c8f79f44dc84534433492fd257ef
Ariss <valentina.ways@yandex.ru>
2021-05-21 18:56:18 +0300

    fix url checks tables

--- resources/views/url/show.blade.php
+++ resources/views/url/show.blade.php
@@ -41,28 +41,30 @@
             {{ Form::close() }}
         <div class="row">
             <div class="col">
-            <table class="table table-hover table-bordered table-responsive">
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">{{ __('Status code') }}</th>
-                        <th scope="col">h1</th>
-                        <th scope="col">keywords</th>
-                        <th scope="col">description</th>
-                        <th scope="col">{{ __('Date of creation') }}</th>
-                    </tr>
-                    @if ($urlChecks)
-                        @foreach ($urlChecks as $urlCheck)
-                            <tr>
-                                <td scope="row">{{ $urlCheck->id }}</td>
-                                <td>{{ $urlCheck->status_code }}</td>
-                                <td>{{ $urlCheck->h1 }}</td>
-                                <td>{{ $urlCheck->keywords }}</td>
-                                <td>{{ $urlCheck->description }}</td>
-                                <td>{{ $urlCheck->created_at }}</td>
-                            </tr>
-                        @endforeach
-                    @endif
-                </table>
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered">
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">{{ __('Status code') }}</th>
+                            <th scope="col">h1</th>
+                            <th scope="col">keywords</th>
+                            <th scope="col">description</th>
+                            <th scope="col">{{ __('Date of creation') }}</th>
+                        </tr>
+                        @if ($urlChecks)
+                            @foreach ($urlChecks as $urlCheck)
+                                <tr>
+                                    <td scope="row">{{ $urlCheck->id }}</td>
+                                    <td>{{ $urlCheck->status_code }}</td>
+                                    <td>{{ $urlCheck->h1 }}</td>
+                                    <td>{{ $urlCheck->keywords }}</td>
+                                    <td>{{ $urlCheck->description }}</td>
+                                    <td>{{ $urlCheck->created_at }}</td>
+                                </tr>
+                            @endforeach
+                        @endif
+                    </table>
+                </div>
             </div>
         </div>
     </div>
