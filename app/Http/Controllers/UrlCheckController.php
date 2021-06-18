b2e668eb2109d734911d2335f5532b4e2f49ec4b
Valentina Vasileva <valentina.ways@yandex.ru>
2021-06-18 14:53:18 +0300

    Check 2. Add class to exception of http client to catch in UrlCheckController

--- app/Http/Controllers/UrlCheckController.php
+++ app/Http/Controllers/UrlCheckController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 use DiDom\Document;
+use Illuminate\Http\Client\HttpClientException;
 
 class UrlCheckController extends Controller
 {
@@ -40,7 +41,7 @@ class UrlCheckController extends Controller
                 ]
             );
             flash(__('messages.Page has been checked successfully'))->success();
-        } catch (\Exception $e) {
+        } catch (HttpClientException $e) {
             flash($e->getMessage())->error();
         }
         return redirect()->route('urls.show', ['url' => $id]);
